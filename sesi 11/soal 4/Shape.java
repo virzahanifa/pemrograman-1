ea6d9bc0aaa150841368d4a3ee4f5ebd245c8eee
virza hanifa <virzahanifa93@gmail.com>
2011-12-07 13:33:23 +0700

    sesi 11

---
+++ sesi 11/soal 4/Shape.java
@@ -0,0 +1,12 @@
+abstract class Shape{
+	public String color;
+	public Shape(){
+	}
+	public void setColor(string c){
+		color = c;
+	}
+	public string getColor(){
+		return color;
+	}
+	abstract public double area();
+}
